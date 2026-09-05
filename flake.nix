154fbfb15cf65af6d24480922202a8d6d6c56cd9
Greg Burd <greg@burd.me>
2026-09-04 23:30:41 -0400

    release: bump flake.nix to 2.2.0 (missed in the graph_ef branch; drift-check doesn't cover it)

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
           postgresql:
           pkgs.buildPgrxExtension {
             pname = "pg_turbovec";
-            version = "2.1.0";
+            version = "2.2.0";
             src = self;
             inherit postgresql cargoLock;
             cargo-pgrx = cargo-pgrx_0_19_1;
